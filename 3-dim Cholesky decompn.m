d1b16a3e894a338737902bc9ced05afa908b351d
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-05 12:02:28 +0800

    Create 3-dim Cholesky decompn.m

---
+++ 3-dim Cholesky decompn.m
@@ -0,0 +1,11 @@
+clc
+clear
+close
+
+syms c12 c13 c23
+assume(c12>=0 & c12<=1)
+assume(c13>=0 & c13<=1)
+assume(c23>=0 & c23<=1)
+assumptions
+A = [1 c12 c13; c12 1 c23; c13 c23 1];
+D = simplify(chol(A,'lower','nocheck'))
