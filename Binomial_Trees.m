725c1db80a96b57889c6a9445b6f07d30a171527
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-05 11:39:32 +0800

    Create Binomial_Trees.m

---
+++ Binomial_Trees.m
@@ -0,0 +1,43 @@
+function Binomial_Value = Binomial(S0,K,r,q,sigma,T,OptionType,ExerciseType,NT)
+dt=T/NT;
+u=exp(sigma*sqrt(dt));
+d=1/u;
+a=exp((r-q)*dt);
+p=(a-d)/(u-d);
+
+f=zeros(NT+1, NT+1);
+for j = 0:NT;
+    if (OptionType=='p')
+        f(NT+1,j+1)=max(K-S0*(u^j)*(d^(NT-j)),0);
+    else
+        f(NT+1,j+1)=max(S0*(u^j)*(d^(NT-j))-K,0);
+    end;
+end;
+
+for i = (NT-1):-1:0;
+    for j = 0:i;
+        if (OptionType=='p')
+            if (ExerciseType=='a')
+                % American put
+                EV=max(K-S0*(u^j)*(d^(i-j)),0);
+                f(i+1,j+1)=max(EV, exp(-r*dt)*(p*f(i+2,j+2)+(1-p)*f(i+2,j+1)));
+            else
+                % European put
+                f(i+1,j+1)= exp(-r*dt)*(p*f(i+2,j+2)+(1-p)*f(i+2,j+1));
+            end;
+        else
+            if (ExerciseType=='a')
+                % American call
+                EV=max(S0*(u^j)*(d^(i-j))-K,0);
+                f(i+1,j+1)=max(EV, exp(-r*dt)*(p*f(i+2,j+2)+(1-p)*f(i+2,j+1)));
+            else
+                % European call
+                f(i+1,j+1)= exp(-r*dt)*(p*f(i+2,j+2)+(1-p)*f(i+2,j+1));
+            end;
+        end;
+    end;
+end;
+Binomial_Value=f(1,1);
+                
+                
+                
