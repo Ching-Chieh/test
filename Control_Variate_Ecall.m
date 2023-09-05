246fd8ed8099bb96da39a4de80045312ed27c3ee
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-05 14:54:57 +0800

    Update Control_Variate_Ecall.m

--- Control_Variate_Ecall.m
+++ Control_Variate_Ecall.m
@@ -9,14 +9,14 @@ T = 5/12;
 sigma = 0.4;
 NRepl = 195000;
 NPilot = 5000;
-S = S0*exp(  (r-0.5*sigma^2)*T+sigma*randn(NPilot,1)*sqrt(T)    );
+S = S0*exp((r-0.5*sigma^2)*T+sigma*randn(NPilot,1)*sqrt(T));
 CallValue = exp(-r*T)*max(S-K,0);
 cov_mat = cov(S,CallValue);
 EY = S0*exp(r*T);
 VarY = S0^2*exp(2*r*T)*(exp(sigma^2*T)-1);
 c = -cov_mat(1,2)/VarY;
 
-Y = S0*exp(  (r-0.5*sigma^2)*T+sigma*randn(NRepl,1)*sqrt(T)    );
+Y = S0*exp((r-0.5*sigma^2)*T+sigma*randn(NRepl,1)*sqrt(T));
 X = exp(-r*T)*max(Y-K,0);
 Xc = X + c*(Y-EY);
 [muHat,sigmaHat,CI] = normfit(Xc);
