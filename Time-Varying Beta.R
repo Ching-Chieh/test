2cfc9db41df0a1ef3e1d6e2d1d5c19d822102149
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-05 20:54:51 +0800

    Update Time-Varying Beta.R

--- Time-Varying Beta.R
+++ Time-Varying Beta.R
@@ -1,40 +1,42 @@
-# Time-varying Beta of TSLA
+# Time-Varying Beta of TSLA
 cat("\014")
 rm(list=ls())
 library(tidyverse)
 library(lubridate)
-quantmod::getSymbols(c('AAPL','TSLA'), from = '2018-01-01')
-aapl <- AAPL %>%
+quantmod::getSymbols(c('^GSPC','TSLA'), from = '2018-01-01')
+sp500 <- GSPC %>%
   as_tibble(rownames = 'date') %>% 
   mutate(date=ymd(date)) %>% 
-  select(date, aapl.close = AAPL.Close)
+  select(date, sp500.close = GSPC.Close)
 tsla <- TSLA %>% 
   as_tibble(rownames = 'date') %>% 
   mutate(date=ymd(date)) %>% 
   select(date, tsla.close = TSLA.Close)
-da <- aapl %>% 
+da <- sp500 %>% 
   left_join(tsla, by = 'date') %>% 
-  drop_na()
-aapl <- log(1 + (da$aapl.close/dplyr::lag(da$aapl.close)-1)[-1])
-tsla <- log(1 + (da$tsla.close/dplyr::lag(da$tsla.close)-1)[-1])
+  drop_na() %>% 
+  mutate(sp500=log(sp500.close/lag(sp500.close)),
+         tsla=log(tsla.close/lag(tsla.close))) %>% 
+  drop_na() %>% 
+  select(date,sp500,tsla)
+rm(list=c('sp500','tsla'))
+sp500=da$sp500
+tsla=da$tsla
+xp=sp500+tsla
+xm=sp500-tsla
 library(fGarch)
-m1 <- garchFit(~1+garch(1,1),data=aapl,trace=F)
-m2 <- garchFit(~1+garch(1,1),data=tsla,trace=F)
-vaapl <- fGarch::volatility(m1)
-vtsla <- fGarch::volatility(m2)
-xp <- aapl+tsla
-xm <- aapl-tsla 
-m3 <- garchFit(~1+garch(1,1),data=xp,trace=F)
-m4 <- garchFit(~1+garch(1,1),data=xm,trace=F)
-vxp <- fGarch::volatility(m3)
-vxm <- fGarch::volatility(m4)
-cov12 <- (vxp^2-vxm^2)/4
-cor12 <- cov12/(vaapl*vtsla)
-df <- tibble(date = da$date[-1],cor12 = cor12)
-df
-ggplot(df, aes(date,cor12)) + 
-  geom_hline(yintercept = cor(aapl,tsla)) + 
+m1=garchFit(~1+garch(1,1),data=xp,trace=F)
+m2=garchFit(~1+garch(1,1),data=xm,trace=F)
+m3=garchFit(~1+garch(1,1),data=sp500,trace=F)
+vxp=fGarch::volatility(m1)
+vxm=fGarch::volatility(m2)
+vsp500=fGarch::volatility(m3)
+beta=(vxp^2-vxm^2)/(4*vsp500^2)
+c2=coef(lm(tsla~sp500))[[2]]
+df <- tibble(date=da$date,beta=beta)
+ggplot(df, aes(date,beta)) + 
+  geom_hline(yintercept = c2) + 
   geom_line(color='red') + 
   scale_x_date(date_breaks = "1 year",date_labels = "%Y") + 
-  labs(y = "correlation",
-       title = 'Time-varying correlations between AAPL and TSLA')
+  labs(y = "Beta",
+       title = 'Time-Varying Beta of TSLA')
