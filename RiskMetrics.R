0d1bc138743dfc4e354b99e5ef4c836fa711d930
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-05 19:09:31 +0800

    Create RiskMetrics.R

---
+++ RiskMetrics.R
@@ -0,0 +1,28 @@
+# Estimate the lambda of RiskMetrics models
+# RiskMetrics's lambda is the value that minimizes the MSE of the variance forecast and
+# the realized variance (subsequent 25 days)
+# 
+cat("\014")
+rm(list=ls())
+library(tidyverse)
+library(readxl)
+library(zoo)   # rollapply
+e <- read_excel("euro.xlsx") |> rename(e = euro)
+f1 <- function(param){
+  lam <- param
+  e |> 
+    mutate(ret = e/lag(e)-1) |>
+    slice({ret1 <<- ret[2]; -(1:2)}) |> 
+    mutate(v =
+             head(
+               accumulate(.x = ret,
+                          .f = ~ .x*lam + (1-lam)*.y^2,
+                          .init = ret1^2),-1)) |> 
+    mutate(beta = rollapply(ret, 25, var, fill=NA, align='left')) |> 
+    mutate(sqdiff = 10^6*(v-beta)^2) |> 
+    slice(26:(n()-24)) |> 
+    summarise(SumSqDiff=sum(sqdiff)) |> 
+    pull()
+}
+res <- optimize(f1, c(0,1))
+res$minimum
