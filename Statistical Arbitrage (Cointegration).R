0d2c1821aa0fcb0b7a0fc0b3610e8473b30599d4
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-10 05:42:34 +0800

    Update Statistical Arbitrage (Cointegration).R

--- Statistical Arbitrage (Cointegration).R
+++ Statistical Arbitrage (Cointegration).R
@@ -1,4 +1,7 @@
-# R program -------------------------------------------------------------------------------
+# Statistical Arbitrage (Cointegrated VAR model)
+# 1. Use R and RATS
+# 2. All using R
+# Step 1: Fetch data using R -------------------------------------------------------------------------------
 cat("\014")
 rm(list=ls())
 library(tidyverse)
@@ -16,8 +19,7 @@ da <- aapl %>%
   left_join(msft, by = 'date') %>% 
   drop_na()
 da %>% write_csv("aaplmsft2011.csv")
-# RATS program -----------------------------------------------------------------------------------
-end(reset)
+# Step 2: Estimate using RATS -----------------------------------------------------------------------------------
 end(reset)
 OPEN DATA "C:\Users\Jimmy\Desktop\aaplmsft2011.csv"
 CALENDAR(D) 2011:1:1
@@ -57,3 +59,27 @@ spgraph(done)
 * Trading strategy
 *  Buy a share of MSFT and short 1.02545 shares of AAPL when spread equals 0.25296 (0.41340 - 0.16043).
 *  Unwind the position when spread equals 0.57383 (0.41340 + 0.16043).
+# All in R ----------------------------------------------------------------------------------------------------
+cat("\014")
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+library(urca)
+da=read_csv("aaplmsft2011.csv")
+sig=stats::sigma(lm(msft~aapl,da))
+xt=as.matrix(select(da,msft,aapl))
+m1=urca::ca.jo(xt,type='eigen',ecdet="const",K=2,spec='transitory')
+gam=-unname(m1@V[,1])[2]
+mu=-unname(m1@V[,1])[3]
+da=mutate(da,spread = msft - gam*aapl)
+upp=mu+sig
+lowe=mu-sig
+cat('Buy a share of MSFT and short ',round(gam,4),' shares of AAPL\n',
+    'when spread equals ',round(upp,4),'\n',
+    'Unwind the position when spread equals ',round(lowe,4),'\n',sep = "")
+cat('The mean of the spread is',round(mu,4),'\n')
+ggplot(da,aes(date,spread)) + 
+  geom_hline(yintercept = upp,color='red') +
+  geom_hline(yintercept = lowe,color='green') +
+  geom_line(color='blue') + 
+  scale_x_date(date_labels = "%Y",date_breaks = "1 year")
