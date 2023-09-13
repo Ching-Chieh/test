29a9b019079db4f9b309610296b4c9bfdcb178da
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-14 05:31:47 +0800

    Update garch11.R

--- garch11.R
+++ garch11.R
@@ -6,7 +6,7 @@ garch11Fit = function(x)
   params = c(mu = Mean, omega = 0.1*Var, alpha = 0.1, beta = 0.8)
   lowerBounds = c(mu = -10*abs(Mean), omega = S^2, alpha = S, beta = S)
   upperBounds = c(mu = 10*abs(Mean), omega = 100*Var, alpha = 1-S, beta = 1-S)
-  garchDist = function(z, hh) { dnorm(x = z/hh)/hh }
+  garchDist = function(z, hh) {dnorm(x = z/hh)/hh}
   garchLLH = function(parm) {
     mu = parm[1]; omega = parm[2]; alpha = parm[3]; beta = parm[4]
     z = (x-mu); Mean = mean(z^2)
@@ -14,11 +14,11 @@ garch11Fit = function(x)
     h = filter(e, beta, "r", init = Mean)
     hh = sqrt(abs(h))
     llh = -sum(log(garchDist(z, hh)))
-    llh }
+    llh
+  }
   print(garchLLH(params))
-  fit = nlminb(start = params, objective = garchLLH,
-               lower = lowerBounds, upper = upperBounds, control = list(trace=3))
-  epsilon = 0.0001 * fit$par
+  fit = stats::nlminb(start = params, objective = garchLLH, lower = lowerBounds, upper = upperBounds, control = list(trace=3))
+  epsilon = 0.0001*fit$par
   Hessian = matrix(0, ncol = 4, nrow = 4)
   for (i in 1:4) {
     for (j in 1:4) {
@@ -27,16 +27,14 @@ garch11Fit = function(x)
       x2[i] = x2[i] + epsilon[i]; x2[j] = x2[j] - epsilon[j]
       x3[i] = x3[i] - epsilon[i]; x3[j] = x3[j] + epsilon[j]
       x4[i] = x4[i] - epsilon[i]; x4[j] = x4[j] - epsilon[j]
-      Hessian[i, j] = (garchLLH(x1)-garchLLH(x2)-garchLLH(x3)+garchLLH(x4))/
-        (4*epsilon[i]*epsilon[j])
+      Hessian[i, j] = (garchLLH(x1)-garchLLH(x2)-garchLLH(x3)+garchLLH(x4))/(4*epsilon[i]*epsilon[j])
     }
   }
-  # stats::optimHess(params,garchLLH)
+  # Hessian = stats::optimHess(params,garchLLH)
   se.coef = sqrt(diag(solve(Hessian)))
   tval = fit$par/se.coef
   matcoef = cbind(fit$par, se.coef, tval, 2*(1-pnorm(abs(tval))))
-  dimnames(matcoef) = list(names(tval), c(" Estimate",
-                                          " Std. Error", " t value", "Pr(>|t|)"))
+  dimnames(matcoef) = list(names(tval), c(" Estimate", " Std. Error", " t value", "Pr(>|t|)"))
   cat("\nCoefficient(s):\n")
   printCoefmat(matcoef, digits = 6, signif.stars = TRUE)
 }
