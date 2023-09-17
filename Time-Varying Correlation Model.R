7ba5fe4fe9229d3da7cc1eb75ae9ffdbeef007fa
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-18 03:58:46 +0800

    Update Time-Varying Correlation Model.R

--- Time-Varying Correlation Model.R
+++ Time-Varying Correlation Model.R
@@ -1,5 +1,6 @@
 # Time-Varying Correlation Model
 # Ruey S. Tsay, Analysis of Financial Time Series, 3th p.527
+# 1 -----------------------------------------------------------------------
 cat("\014")
 rm(list=ls())
 library(tidyverse)
@@ -38,6 +39,8 @@ logl <- function(parm) {
   for (t in 4:N) {
     a1[t]=ibm[t]-p10-p11*ibm[t-1]-p12*sp500[t-2]
     a2[t]=sp500[t]-p20
+    # h1[t]=c1+a11*a1[t-1]^2              +b11*h1[t-1]+b12*h2[t-1]
+    # h2[t]=c2+a21*a1[t-1]^2+a22*a2[t-1]^2+b21*h1[t-1]+b22*h2[t-1]
     h1[t]=abs(c1+a11*a1[t-1]^2              +b11*h1[t-1]+b12*h2[t-1])
     h2[t]=abs(c2+a21*a1[t-1]^2+a22*a2[t-1]^2+b21*h1[t-1]+b22*h2[t-1])
     q[t]=q0+q1*rho[t-1]+q2*a1[t-1]*a2[t-1]/sqrt(h1[t-1]*h2[t-1])
@@ -76,25 +79,25 @@ pp1=round(pp,3)
 pp1[1:3]
 pp1[4]
 pp1[5:6]
-matrix(c(pp1[7:8],0,pp1[9]),2)
-matrix(c(pp1[10:13]),2)
+matrix(c(pp1[7:8],0,pp1[9]),2,byrow = T)
+matrix(c(pp1[10:13]),2,byrow = T)
 pp1[14:16]
-
-# epsilon = 0.0001*fit$par
-# npar=length(params)
-# Hessian = matrix(0, ncol = npar, nrow = npar)
-# for (i in seq_along(npar)) {
-#   for (j in seq_along(npar)) {
-#     x1 = x2 = x3 = x4 = fit$par
-#     x1[i] = x1[i] + epsilon[i]; x1[j] = x1[j] + epsilon[j]
-#     x2[i] = x2[i] + epsilon[i]; x2[j] = x2[j] - epsilon[j]
-#     x3[i] = x3[i] - epsilon[i]; x3[j] = x3[j] + epsilon[j]
-#     x4[i] = x4[i] - epsilon[i]; x4[j] = x4[j] - epsilon[j]
-#     Hessian[i, j] = (logl(x1)-logl(x2)-logl(x3)+logl(x4))/
-#       (4*epsilon[i]*epsilon[j])
-#   }
-# }
-Hessian = stats::optimHess(params,logl)
+# 2 -----------------------------------------------------------------------
+epsilon = 0.0001*fit$par
+npar=length(params)
+Hessian = matrix(0, ncol = npar, nrow = npar)
+for (i in seq_along(npar)) {
+  for (j in seq_along(npar)) {
+    x1 = x2 = x3 = x4 = fit$par
+    x1[i] = x1[i] + epsilon[i]; x1[j] = x1[j] + epsilon[j]
+    x2[i] = x2[i] + epsilon[i]; x2[j] = x2[j] - epsilon[j]
+    x3[i] = x3[i] - epsilon[i]; x3[j] = x3[j] + epsilon[j]
+    x4[i] = x4[i] - epsilon[i]; x4[j] = x4[j] - epsilon[j]
+    Hessian[i, j] = (logl(x1)-logl(x2)-logl(x3)+logl(x4))/
+      (4*epsilon[i]*epsilon[j])
+  }
+}
+# Hessian = stats::optimHess(params,logl)
 stderr = sqrt(diag(solve(Hessian)))
 data.frame(
   term=names(params),
