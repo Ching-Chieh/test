81d130ec0e7d4b747d0c5f99a5742c11baf22a24
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2023-09-26 13:28:39 +0800

    Update CIR CKLS.R

--- CIR CKLS.R
+++ CIR CKLS.R
@@ -5,6 +5,15 @@ library(yuima)
 quantmod::getSymbols("DEXUSEU", src="FRED")
 meanCIR <- mean(DEXUSEU, na.rm=TRUE)
 DEXUSEU <- setData(na.omit(DEXUSEU),delta=1/252)
+# Vasicek
+vasicek <- setModel(drift="theta1-theta2*x", diffusion="sigma")
+mod.vasicek <- setYuima(model=vasicek, data=DEXUSEU)
+vasicek.fit <- qmle(mod.vasicek,
+                    start=list(theta1=1, theta2=1, sigma=0.5),
+                    lower=list(theta1=0.1, theta2=0.1, sigma=0.1),
+                    upper=list(theta1=10, theta2=10, sigma=100),
+                    method="L-BFGS-B")
+summary(vasicek.fit)
 # CIR
 cir <- setModel(drift="kappa*(mu-x)", diffusion="sigma*sqrt(x)")
 mod.cir <- setYuima(model=cir, data=DEXUSEU)
@@ -24,4 +33,4 @@ ckls.fit <- qmle(mod.ckls,
                  upper=list(theta1=10, theta2=10, sigma=10,gamma=2),
                  method="L-BFGS-B")
 summary(ckls.fit)
-AIC(cir.fit,ckls.fit)
+AIC(vasicek.fit,cir.fit,ckls.fit)
