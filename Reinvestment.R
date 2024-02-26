111542aff678e6496ba525f7a408137d9c2542bf
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2024-02-26 17:53:02 +0800

    Update Reinvestment.R

--- Reinvestment.R
+++ Reinvestment.R
@@ -19,12 +19,16 @@ dividends <- umc.dividends %>%
   as_tibble(rownames = 'date') %>% 
   rename(dividends = '2303.TW.div') %>% 
   mutate(date = ymd(date))
+# check missing values
+map_dbl(p, ~sum(is.na(.x)))
+map_dbl(dividends, ~sum(is.na(.x)))
 # check price at ex-dividend day: The 'close' at ex-dividend day already minuses dividend.
 p %>% filter(date %in% (ymd('2010-07-08') + -1:0))
 dividends %>% filter(date == ymd('2010-07-08'))
 
 df <- p %>% 
   inner_join(dividends, by = 'date')
+map_dbl(df, ~sum(is.na(.x)))  # check missing values
 rm(list = c('dividends'))
 p0 <- p$price[1]  # price at 2009-01-02
 rm(list = c('p'))
@@ -39,7 +43,8 @@ table_1 <- df %>%
          total_value = price*N)
 invest_horizon <- as.numeric(slice_tail(table_1, n = 1)$date - ymd('2009-01-02')) / 365
 annual_return = (slice_tail(table_1, n = 1)$total_value / C)^(1/invest_horizon) - 1
-annual_return
+# annualized rate of return is 22.8%
+# initial 1,000,000 -> final 19,673,715
 # accumulate2 -----------------------------------------------------------------------
 cat("\014")
 rm(list=ls())
