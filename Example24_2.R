53a5f8c1d590941268e03ed217c072e5df6fca55
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2024-03-08 05:09:54 +0800

    Create Example24_2.R

---
+++ Example24_2.R
@@ -0,0 +1,45 @@
+# Example 24.2 -----------------------------------------------------------------------
+cat("\014")
+rm(list=ls())
+library(tidyverse)
+r = 0.05
+y = c(0.065,0.068,0.0695)
+rf_price <- function(t) {
+  tt = seq(0.5,t,0.5)
+  cf = rep(4, 2*t)
+  cf[length(cf)] = cf[length(cf)] + 100
+  sum(cf*exp(-r*tt))
+}
+price <- function(t) {
+  tt = seq(0.5,t,0.5)
+  cf = rep(4, 2*t)
+  cf[length(cf)] = cf[length(cf)] + 100
+  sum(cf*exp(-y[t]*tt))
+}
+brf = map_dbl(1:3, rf_price)
+b = map_dbl(1:3, price)
+round(b,2)
+round(brf,2)
+default_loss = brf - b
+NDV <- function(t) {
+  rev(accumulate(rep(4*exp(-r*0.25),t*2-1), ~.x*exp(-r*0.5) + .y, .init = 104*exp(-r*0.25)))
+}
+tf <- function(t) seq(0.25, by = 0.5, length.out = 2*t)
+pvloss = map(1:3, ~(NDV(.x) - 40)*exp(-r*tf(.x)))
+##
+lambda <- c(0.02, 0.03, 0.05)
+for (i in 1:3) {
+  f <- function(lamb) {
+    lambda[i] <- lamb
+    lambda = rep(lambda, each = 2)
+    ps = cumprod(exp(-0.5*lambda))
+    pd = c(1 - ps[1], -diff(ps))
+    expect_loss = function(t){
+      sum(pvloss[[t]]*pd[1:(2*t)])
+    }
+    EL = map_dbl(1:3, expect_loss)  
+    default_loss[i] - EL[i]
+  }
+  lambda[i] <- uniroot(f, interval = c(0,1))$root
+}
+round(lambda*100,2)
