b29e7476a61837bb08f28e8b81e6a21c52a745de
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2024-03-24 12:25:53 +0800

    Create data_for_RATS.R

---
+++ Time Series/Statistical Arbitrage Pairs Trading (Cointegration)/data_for_RATS.R
@@ -0,0 +1,17 @@
+cat("\014")
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+quantmod::getSymbols(c('AAPL','MSFT'), from = '2011-01-01')
+aapl <- AAPL %>% 
+  as_tibble(rownames = 'date') %>% 
+  select(date,aapl=AAPL.Close) %>% 
+  mutate(date=ymd(date),aapl=log(aapl))
+msft <- MSFT %>%
+  as_tibble(rownames = 'date') %>%
+  select(date,msft=MSFT.Close) %>% 
+  mutate(date=ymd(date),msft=log(msft))
+da <- aapl %>% 
+  left_join(msft, by = 'date') %>% 
+  drop_na()
+da %>% write_csv("aaplmsft2011.csv")
