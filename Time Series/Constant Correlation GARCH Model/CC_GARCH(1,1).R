6bd5dc909b910dd7e245a4b32cc95ab5697c5950
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2024-04-10 01:16:41 +0800

    Update CC_GARCH(1,1).R

--- Time Series/Constant Correlation GARCH Model/CC_GARCH(1,1).R
+++ Time Series/Constant Correlation GARCH Model/CC_GARCH(1,1).R
@@ -1,7 +1,7 @@
 # Constant Conditional Correlation GARCH(1,1) -----------------------------------------------------------------------
 cat("\014")
 rm(list=ls())
-da=unname(as.matrix(read.table('d-spcscointc.txt', header = TRUE)))
+da = unname(as.matrix(read.table('d-spcscointc.txt', header = TRUE)))
 T = nrow(da)  # number of timepoints
 N = ncol(da)  # dimension of vector, number of assets
 
@@ -16,48 +16,45 @@ nlogl <- function(params) {
   qc = diag(params[(4*N+1):(5*N-1)]) # correlation matrix
   qc[lower.tri(qc)] = params[(5*N):(5*N+(N-1)*(N-2)/2-1)]
   for (t in 2:T) {
+    # fill matrix h's diagonal
     for (i in 1:N) {
-      # fill matrix h's diagonal 
-      h[i,i,t] = 
-        vcv[i,1] + vav[i,1]*tcrossprod(u[t-1,])[i,i] + vbv[i,1]*h[i,i,t-1]
-      # fill matrix h's entries below diagonal
-      if (i != 1) {
-        for (j in 1:(i-1)) {
-          h[i,j,t] = qc[i-1,j]*sqrt(h[i,i,t]*h[j,j,t])
-        }
+      h[i,i,t] = vcv[i,1] + vav[i,1]*tcrossprod(u[t-1,])[i,i] + vbv[i,1]*h[i,i,t-1]
+    }
+    # fill matrix h's entries below diagonal
+    for (i in 2:N) {
+      for (j in 1:(i-1)) {
+        h[i,j,t] = qc[i-1,j]*sqrt(h[i,i,t]*h[j,j,t])
       }
-      # fill matrix h's entries above diagonal 
-      
-      # method 1
-      # tmp = h[,,t]
-      # tmp[upper.tri(tmp)] = tmp[lower.tri(tmp)]
-      # h[,,t] = tmp
-      
-      # method 2
-      for (i in 1:(N-1)) {
-        for (j in (i+1):N) {
-          h[i,j,t] = h[j,i,t]
-        }
+    }
+    # fill matrix h's entries above diagonal 
+    
+    # method 1
+    # tmp = h[,,t]
+    # tmp[upper.tri(tmp)] = tmp[lower.tri(tmp)]
+    # h[,,t] = tmp
+    
+    # method 2
+    for (ii in 1:(N-1)) {
+      for (jj in (ii+1):N) {
+        h[ii,jj,t] = h[jj,ii,t]
       }
     }
   }
-  llv = numeric(T)
-  for (t in 1:T) {
-    llv[i] = c(t(u[t,])%*%solve(h[,,t])%*%u[t,])
-  }
+  llv = sapply(1:T, \(t) c(t(u[t,])%*%solve(h[,,t])%*%u[t,]))
   # llv = sum(-N/2*log(2*pi) - 0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
-  # llv = sum(-0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
-  llv = sum(-(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
+  llv = sum(-0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
+  # llv = sum(-(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
   cnt <<- cnt + 1
   return(-llv)
 }
 # set initial values
-# rr = da - matrix(1,T,1)%*%matrix(apply(da,2,mean),1,N)
-rr = cov(scale(da, scale = FALSE))
-h = array(rr, c(N,N,T))
+# da - matrix(1,T,1)%*%matrix(apply(da,2,mean),1,N)
+# scale(da, scale = FALSE)
+h = array(0, c(N,N,T))
+h[,,1] = cov(da)
 start_values = c(
   apply(da,2,mean),  # mean
-  diag(rr),          # vcv
+  diag(cov(da)),     # vcv
   rep(0.05, N),      # vav
   rep(0.8, N),       # vbv
   rep(0,(N-1)*N/2)   # correlations  (2,1) (3,2) (3,1)
@@ -70,14 +67,14 @@ mm = nlminb(start_values, nlogl,
               rep(S,N),                   # vcv
               rep(S,N),                   # vav
               rep(S,N),                   # vbv
-              rep(-1+S,N)                 # correlations  (2,1) (3,2) (3,1)
+              rep(-1+S,(N-1)*N/2)         # correlations  (2,1) (3,2) (3,1)
             ),
             upper = c(
               20*abs(apply(da,2,mean)),   # mean
-              20*diag(rr),                # vcv
+              20*diag(cov(da)),           # vcv
               rep(1-S,N),                 # vav
               rep(1-S,N),                 # vbv
-              rep(1-S,N)                  # correlations  (2,1) (3,2) (3,1)
+              rep(1-S,(N-1)*N/2)          # correlations  (2,1) (3,2) (3,1)
             )
 )
 
@@ -93,21 +90,20 @@ names(pars) = c(
   'rho(1,2)','rho(2,3)', 'rho(1,3)'
 )
 pars
-# Use SAS' estimators to calculate log likelihood -----------------------------------------------------------------------
+# Use SAS' estimators to replicate SAS' log likelihood -----------------------------------------------------------------------
 cat("\014")
 rm(list=ls())
 da=unname(as.matrix(read.table('d-spcscointc.txt', header = TRUE)))
 T = nrow(da)
 N = ncol(da)
-rr = cov(scale(da, scale = FALSE))
-h = array(rr, c(N,N,T))
-# SAS' estimators
+h = array(0, c(N,N,T))
+h[,,1] = cov(da)
 params = c(
   0.07074, 0.33455, 0.20797, # mean
   0.00818, 0.18336, 0.04149, # vcv
   0.04320, 0.06187, 0.01178, # vav
   0.94428, 0.91661, 0.98135, # vbc
-  0.51954, 0.47771, 0.48626  # correlations  (2,1) (3,2) (3,1)
+  0.51954, 0.47771, 0.48626
 )
 u = da - matrix(1,T,1)%*%matrix(params[1:N],1,N)
 vcv = matrix(params[(N+1):(2*N)],N,1)
@@ -116,54 +112,31 @@ vbv = matrix(params[(3*N+1):(4*N)],N,1)
 qc = diag(params[(4*N+1):(5*N-1)]) # correlation matrix
 qc[lower.tri(qc)] = params[(5*N):(5*N+(N-1)*(N-2)/2-1)]
 for (t in 2:T) {
-  cat('*** t =', t, '\n')
+  # fill matrix h's diagonal
   for (i in 1:N) {
-    cat('*** i =    ', i, '\n')
-    # fill matrix h's diagonal 
-    h[i,i,t] = 
-      vcv[i,1] + vav[i,1]*tcrossprod(u[t-1,])[i,i] + vbv[i,1]*h[i,i,t-1]
-    # fill matrix h's entries below diagonal
-    if (i != 1) {
-      for (j in 1:(i-1)) {
-        h[i,j,t] = qc[i-1,j]*sqrt(h[i,i,t]*h[j,j,t])
-      }
+    h[i,i,t] = vcv[i,1] + vav[i,1]*tcrossprod(u[t-1,])[i,i] + vbv[i,1]*h[i,i,t-1]
+  }
+  # fill matrix h's entries below diagonal
+  for (i in 2:N) {
+    for (j in 1:(i-1)) {
+      h[i,j,t] = qc[i-1,j]*sqrt(h[i,i,t]*h[j,j,t])
     }
-    # fill matrix h's entries above diagonal
-    
-    # method 1
-    # tmp = h[,,t]
-    # tmp[upper.tri(tmp)] = tmp[lower.tri(tmp)]
-    # h[,,t] = tmp
-    
-    # method 2
-    for (i in 1:(N-1)) {
-      for (j in (i+1):N) {
-        h[i,j,t] = h[j,i,t]
-      }
+  }
+  # fill matrix h's entries above diagonal 
+  
+  # method 1
+  # tmp = h[,,t]
+  # tmp[upper.tri(tmp)] = tmp[lower.tri(tmp)]
+  # h[,,t] = tmp
+  
+  # method 2
+  for (ii in 1:(N-1)) {
+    for (jj in (ii+1):N) {
+      h[ii,jj,t] = h[jj,ii,t]
     }
   }
 }
-llv = numeric(T)
-for (t in 1:T) {
-  llv[i] = c(t(u[t,])%*%solve(h[,,t])%*%u[t,])
-}
-sum(-N/2*log(2*pi) - 0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
-sum(-(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv)) # closest to SAS' log likelihood
-# 2 -----------------------------------------------------------------------
-cat("\014")
-rm(list=ls())
-idx = matrix(c(11:14,21:24,31:34,41:44), 4, byrow=TRUE)
-
-idx[lower.tri(idx)] = c(111,222,333,444,555,666)
-
-idx = matrix(c(11:14,21:24,31:34,41:44), 4, byrow=TRUE)
-m = idx + 10
-m[1,2:4] = 0
-m[2,3:4] = 0
-m[3,4] = 0
-m  # 21 stands for correlation between asset 1 and asset 2
-   # 32 stands for correlation between asset 2 and asset 3
-# When we go from params' left to right
-# first 4 is diagonal 21,32,43,54     and then 31,41,51,  42,52,  53
-# Python is different: from left to right and then go to the next row
-# When dimension>=5 this problem arises.
+llv = sapply(1:T, \(t) c(t(u[t,])%*%solve(h[,,t])%*%u[t,]))
+# sum(-N/2*log(2*pi) - 0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
+# sum(-(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
+sum(-0.5*(log(sapply(seq(dim(h)[3]), \(t) det(h[,,t]))) + llv))
