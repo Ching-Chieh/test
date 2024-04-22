909a8e0a7be2a66a19b8445655d158cff83b0af8
Ching-Chieh <144081670+Ching-Chieh@users.noreply.github.com>
2024-04-22 21:33:21 +0800

    Update Symmetric_matrix.R

--- Symmetric_matrix.R
+++ Symmetric_matrix.R
@@ -1,13 +1,25 @@
-# fill a symmetric matrix -------------------------------------------------
+# symmetric matrix --------------------------------------------------------
 cat("\014")
 rm(list=ls())
-# wrong method
+# 1. fill upper *******************************
+x = matrix(0,4,4)
+x[upper.tri(x)] = 1:6
+# 2. fill lower
+x[lower.tri(x)] = t(x)[lower.tri(x)]
+isSymmetric.matrix(x)
+
+# 1. fill lower *******************************
+x = matrix(0,4,4)
+x[lower.tri(x)] = 1:6
+# 2. fill upper *******************************
+x[upper.tri(x)] = t(x)[upper.tri(x)]
+isSymmetric.matrix(x)
+
+
+# wrong method *********************************
 x = matrix(0,4,4)
 x[upper.tri(x)] = 1:6
 x[lower.tri(x)] = x[upper.tri(x)]
-isSymmetric.matrix(x)
-# right method
-x = matrix(0,4,4)
-x[upper.tri(x)] = 1:6
-x[lower.tri(x)] = t(x)[lower.tri(x)]
+# x[lower.tri(x)] = 1:6
+# x[upper.tri(x)] = x[lower.tri(x)]
 isSymmetric.matrix(x)
